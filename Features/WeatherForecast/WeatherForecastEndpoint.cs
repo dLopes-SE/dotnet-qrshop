14b4f87568b4aa7a2da74d01f432c3bf1707061e
Dylan Lopes <dylananlopes@gmail.com>
2025-05-14 17:06:22 +0100

    added carter for endpoint mapping

---
+++ Features/WeatherForecast/WeatherForecastEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Carter;
+using dotnet_qrshop.Common.Messaging;
+
+namespace dotnet_qrshop.Features.WeatherForecast;
+
+public class WeatherForecastEndpoint : ICarterModule
+{
+  public void AddRoutes(IEndpointRouteBuilder app)
+  {
+    app.MapGet("/weatherforecast", async (
+      IQueryHandler<WeatherForecastQuery, WeatherForecastResponse> handler,
+      CancellationToken cancellationToken) =>
+    {
+      var query = new WeatherForecastQuery();
+
+      var result = await handler.Handle(query, cancellationToken);
+
+      return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest();
+    })
+    .WithName("GetWeatherForecast");
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
+using Carter;
 using dotnet_qrshop.Common.Messaging;
-using dotnet_qrshop.Features.WeatherForecast;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Carter for endpoint mapping
+builder.Services.AddCarter();
+
+
+// Add Custom Mediator DI
 builder.Services.Scan(scan => scan.FromAssembliesOf(typeof(IQueryHandler<,>))
             .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
                 .AsImplementedInterfaces()
@@ -23,21 +28,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", async (
-  IQueryHandler<WeatherForecastQuery, WeatherForecastResponse> handler,
-  CancellationToken cancellationToken) =>
-{
-  var query = new WeatherForecastQuery();
-
-  var result = await handler.Handle(query, cancellationToken);
-
-  return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest();
-})
-.WithName("GetWeatherForecast");
+app.MapCarter();
 
 app.Run();
