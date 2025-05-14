141cf2f7e44531e8fbebe01b886f166264d69058
Dylan Lopes <dylananlopes@gmail.com>
2025-05-14 16:09:39 +0100

    Added Result pattern

---
+++ Common/Results/Error.cs
@@ -0,0 +1,32 @@
+﻿namespace dotnet_qrshop.Common.Results;
+
+public record Error
+{
+  public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+  public static readonly Error NullValue = new(
+    "General.Null",
+    "Null value was provided",
+    ErrorType.Failure);
+
+  public string Code { get; }
+  public string Description { get; }
+  public ErrorType ErrorType { get; }
+  public Error(string code, string description, ErrorType type)
+  {
+    Code = code;
+    Description = description;
+    ErrorType = type;
+  }
+
+  public static Error Failure(string code, string description) =>
+    new(code, description, ErrorType.Failure);
+
+  public static Error NotFound(string code, string description) =>
+    new(code, description, ErrorType.NotFound);
+
+  public static Error Problem(string code, string description) =>
+    new(code, description, ErrorType.Problem);
+
+  public static Error Conflict(string code, string description) =>
+    new(code, description, ErrorType.Conflict);
+}
