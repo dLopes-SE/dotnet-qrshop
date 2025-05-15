8c06a945d3323ebb42771f860eb3f1f90f4e5a1a
Dylan Lopes <dylananlopes@gmail.com>
2025-05-15 09:05:51 +0100

    added identity and authentication

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 using Carter;
 using dotnet_qrshop.Common.Messaging;
+using dotnet_qrshop.Common.Models;
+using dotnet_qrshop.Domain;
+using dotnet_qrshop.Persistence.DbContext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +17,6 @@ builder.Services.AddOpenApi();
 
 // Add Carter for endpoint mapping
 builder.Services.AddCarter();
-
 
 // Add Custom Mediator DI
 builder.Services.Scan(scan => scan.FromAssembliesOf(typeof(IQueryHandler<,>))
@@ -22,6 +29,36 @@ builder.Services.Scan(scan => scan.FromAssembliesOf(typeof(IQueryHandler<,>))
             .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
                 .AsImplementedInterfaces()
                 .WithScopedLifetime());
+
+// Identity
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+  .AddEntityFrameworkStores<UserDbContext>()
+  .AddDefaultTokenProviders();
+
+// Authentication
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+  o.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true,
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ClockSkew = TimeSpan.Zero,
+    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
+  };
+});
 
 var app = builder.Build();
 
