3d5d25fd5e11f74711eff9a81ba9485924d1ac2f
Dylan Lopes <dylananlopes@gmail.com>
2025-06-28 11:24:08 +0100

    Added login/register validators

---
+++ src/Features/Identity/Login/LoginUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace dotnet_qrshop.Features.Identity.Login;
+
+public class LoginUserValidator : AbstractValidator<LoginUserCommand>
+{
+  public LoginUserValidator()
+  {
+    RuleFor(u => u.Request.Email)
+      .NotNull()
+      .NotEmpty()
+      .EmailAddress()
+      .WithMessage("Must enter a valid email");
+
+    RuleFor(u => u.Request.Password)
+      .NotNull()
+      .NotEmpty()
+      .WithMessage("Must enter the password");
+  }
+}
---
+++ src/Features/Identity/Register/RegisterUserValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace dotnet_qrshop.Features.Identity.Register;
+
+public class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
+{
+  public RegisterUserValidator()
+  {
+    RuleFor(u => u.Request.Name)
+      .NotNull()
+      .NotEmpty()
+      .WithMessage("Must enter a name.");
+
+    RuleFor(u => u.Request.Email)
+      .NotNull()
+      .NotEmpty()
+      .EmailAddress()
+      .WithMessage("Must enter a valid email");
+
+    RuleFor(u => u.Request.Password)
+      .NotNull()
+      .NotEmpty()
+      .WithMessage("Must enter the password");
+  }
+}
