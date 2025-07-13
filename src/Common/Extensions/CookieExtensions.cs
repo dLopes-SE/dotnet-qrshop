5a90bcb7b9e162c8668932811da14a36af05174b
Dylan Lopes <dylananlopes@gmail.com>
2025-07-13 13:22:18 +0100

    Implemented cookie extension

---
+++ src/Common/Extensions/CookieExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace dotnet_qrshop.Common.Extensions;
+
+public static class CookieExtensions
+{
+
+  public static void SetCookie(this HttpResponse response, string key, string value, TimeSpan? lifetime = null)
+  {
+    response.Cookies.Append(key, value, new CookieOptions
+    {
+      HttpOnly = true,
+      Secure = true,
+      SameSite = SameSiteMode.Strict,
+      MaxAge = lifetime ?? TimeSpan.FromDays(7)
+    });
+  }
+
+  public static string? GetCartHash(this HttpRequest request, string key)
+  {
+    return request.Cookies.TryGetValue(key, out var value) ? value : null;
+  }
+}
