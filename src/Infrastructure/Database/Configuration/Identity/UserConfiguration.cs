cb4b8f7125904d46fe16e75d2d0a84ee3a50e6e0
Dylan Lopes <dylananlopes@gmail.com>
2025-07-17 12:22:55 +0100

    Updated user configuration - changed passwords

--- src/Infrastructure/Database/Configuration/Identity/UserConfiguration.cs
+++ src/Infrastructure/Database/Configuration/Identity/UserConfiguration.cs
@@ -17,7 +17,7 @@ public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
         Name = "System Admin",
         UserName = "admin@localhost.com",
         NormalizedUserName = "ADMIN@LOCALHOST.COM",
-        PasswordHash = "AQAAAAIAAYagAAAAEH1nkGC0NgsEVoDkJH7tzlS7s0MkO91QcDinx/E0ENy4v/Q7srHiwnJmMyUiXOVV6Q==",
+        PasswordHash = "AQAAAAIAAYagAAAAEO2tJRmTOwg5nGQui7zhUrl3CHIuWDTDFYAuMJsYRejkAIbQVeXDasobhj8hyTijsQ==",
         ConcurrencyStamp = "04f197f7-1f0a-41f9-91f1-b7933076cf0b",
         SecurityStamp = "00dc5147-c14c-47df-8c69-31e97241aae2",
         EmailConfirmed = true
@@ -30,7 +30,7 @@ public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
         Name = "System User",
         UserName = "user@localhost.com",
         NormalizedUserName = "USER@LOCALHOST.COM",
-        PasswordHash = "AQAAAAIAAYagAAAAEH1nkGC0NgsEVoDkJH7tzlS7s0MkO91QcDinx/E0ENy4v/Q7srHiwnJmMyUiXOVV6Q==",
+        PasswordHash = "AQAAAAIAAYagAAAAEO2tJRmTOwg5nGQui7zhUrl3CHIuWDTDFYAuMJsYRejkAIbQVeXDasobhj8hyTijsQ==",
         ConcurrencyStamp = "36614143-1fd7-4414-ac79-e5c38a46c1d9",
         SecurityStamp = "2ed6dccd-c411-4f1a-9c2d-a71a3812ad3b",
         EmailConfirmed = true
