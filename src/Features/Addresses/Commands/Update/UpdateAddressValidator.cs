17125204013162ec22aace9178e87a1e43064239
Dylan Lopes <dylananlopes@gmail.com>
2025-08-10 10:33:54 +0100

    implemented updateAddress validator

---
+++ src/Features/Addresses/Commands/Update/UpdateAddressValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace dotnet_qrshop.Features.Addresses.Commands.Update;
+
+public class UpdateAddressValidator : AbstractValidator<UpdateAddressCommand>
+{
+  public UpdateAddressValidator()
+  {
+    RuleFor(a => a.request)
+      .SetValidator(new AddressValidator());
+  }
+}
